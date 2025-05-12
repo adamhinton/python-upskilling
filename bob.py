d634c78b97a59f40539eb040773e9afe554a89f8
adamhinton <adam.d.hinton@gmail.com>
2025-05-12 16:08:24 -0400

    WIP config stuff

---
+++ bob.py
@@ -0,0 +1,13 @@
+print("If you're seeing this, Python afdsfsafsafst shitting the bed.")
+
+def    foo ():   return     'hello'
+
+
+x = 1
+x = 2
+
+def takes_str(x: str) -> None:
+    print(x.upper())
+
+takes_str(123)
+
