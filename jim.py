057245131e1a242739a6431b37d9487a6b6caf1e
adamhinton <adam.d.hinton@gmail.com>
2025-05-13 19:07:36 -0400

    WIP setup

--- jim.py
+++ jim.py
@@ -2,13 +2,15 @@ print("If you're seeing this, Python afdsfsafsafst shitting the bed.")
 
 
 def foo():
-    return "hellofdsfdsafsafsad"
+    return "hello"
 
 
 x = 1
 x = 2
-x = 3
 
 
 def takes_str(x: str) -> None:
     print(x.upper())
+
+
+takes_str(123)
---
+++ template.py
@@ -0,0 +1,14 @@
+# ______________________________________________________
+# Starter template for coding problems.
+# ______________________________________________________
+
+# from typing import List
+
+
+# def solution(nums: List[int]) -> int:
+#     pass
+
+
+# if name == "main":
+#     print(solution([1, 2, 3]))
+#     print("✅")
