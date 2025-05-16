782a93e46fdae203b8db2657f8dc4fc5288925a5
adamhinton <adam.d.hinton@gmail.com>
2025-05-16 00:13:00 -0400

    skeleton group_anagrams

--- problems/group-anagrams.py
+++ problems/group-anagrams.py
@@ -1,1 +1,13 @@
 # https://www.codewars.com/kata/537400e773076324ab000262/train/python
+
+from typing import Dict, List, Tuple  # noqa: F401
+
+def group_anagrams(words: List[str]) -> List[List[str]]:
+    solution = []
+
+    for word in words:
+        print(word)
+
+    return solution
+
+print(group_anagrams(["tsar", "rat", "tar", "star", "tars", "cheese"]))
