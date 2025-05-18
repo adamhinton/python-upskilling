2e7f282de7e893cb80da3bf2645ca02d2e4a5b67
adamhinton <adam.d.hinton@gmail.com>
2025-05-18 00:20:17 -0400

    solution works on example case

--- problems/best-time-buy-sell-stock.py
+++ problems/best-time-buy-sell-stock.py
@@ -1,9 +1,39 @@
-from typing import Dict, List, Tuple  # noqa: F401
+from typing import List
 
 def max_profit(prices: List[int]) -> int:
-    
-    #Placeholder
-    return 0
+    buy_pointer = 0
+    sell_pointer = 1
+
+    lowest_buy_price = prices[0]
+    highest_sell_price = prices[1]
+
+    prices_len = len(prices)
+
+    # Don't think I need to include check on buy_pointer here
+    while sell_pointer < prices_len:
+        current_buy_price = prices[buy_pointer]
+        current_sell_price = prices[sell_pointer]
+
+        # Sell_pointer stuff
+        if current_sell_price > highest_sell_price:
+            highest_sell_price = current_sell_price
+        
+        # I think I can increment it on every loop? Not doing it this time
+        # Right now this doesn't change sell_pointer if it's currently the best oen
+        elif sell_pointer < prices_len:
+            sell_pointer += 1
+        
+
+        if current_buy_price < lowest_buy_price:
+            lowest_buy_price = current_buy_price
+        
+        # Doesn't change buy_pointer if it's currently the best one
+        elif buy_pointer < (sell_pointer - 1):
+            buy_pointer += 1
+
+    return highest_sell_price - lowest_buy_price
+
+print(max_profit([3, 10, 8, 4])) # 7
 
 # PLAN:
 
