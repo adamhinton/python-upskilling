f4e9ac2748e77fcb91edf5759c1e18678d67a6e8
adamhinton <adam.d.hinton@gmail.com>
2025-05-18 00:09:36 -0400

    pseudocode plan

--- problems/best-time-buy-sell-stock.py
+++ problems/best-time-buy-sell-stock.py
@@ -1,7 +1,39 @@
 from typing import Dict, List, Tuple  # noqa: F401
 
-def max_profit(prices):
-    pass
+def max_profit(prices: List[int]) -> int:
+    
+    #Placeholder
+    return 0
+
+# PLAN:
+
+# Two pointers starting at index 0 and 1
+# buy can never be equal to or greater than sell
+# buy_pointer = 0
+# sell_pointer = 1
+
+# lowest_buy_price = 0
+# highest_sell_price = 0
+
+# prices_len = len(prices)
+
+# while sell_pointer < prices_len AND buy_pointer < (prices_len - 1):
+    # current_buy_price = prices[buy_pointer]
+    # current_sell_price = prices[sell_pointer]
+
+    # if current_sell_price > highest_sell_price:
+        # highest_sell_price = current_sell_price
+    # Doesn't change sell_pointer if it's currently the best one
+    # elif sell_pointer < prices_len:
+        # sell_pointer++
+
+    # if current_buy_price < lowest_buy_price:
+        # lowest_buy_price = current_buy_price
+    # Doesn't change buy_pointer if it's currently the best one
+    # elif buy_pointer < (sell_pointer < 1):
+        # buy_pointer++
+
+# return highest_sell_price - lowest_buy_price
 
 
 
