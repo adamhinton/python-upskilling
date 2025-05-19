80a5dbc592b6aceac36da34fc6f97f476720a41d
adamhinton <adam.d.hinton@gmail.com>
2025-05-19 05:14:13 -0400

    skeleton move_zeros

---
+++ problems/section-1/move-zeros.py
@@ -0,0 +1,10 @@
+# Write an algorithm that takes an array
+#  and moves all of the zeros to the end, 
+# preserving the order of the other elements.
+
+from typing import List
+
+def move_zeros(lst: List[int]) -> List[int]:
+
+    # Dummy placeholder
+    return [0]
