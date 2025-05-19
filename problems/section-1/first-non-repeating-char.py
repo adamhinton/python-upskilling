fa882b3ae3930551119f8fe1d5089fa0ae180d9c
adamhinton <adam.d.hinton@gmail.com>
2025-05-19 05:47:53 -0400

    Skeleton first_non_repeating_letter

---
+++ problems/section-1/first-non-repeating-char.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+
+# Instructions are too long to copy here, but basically:
+# Take string input
+# Return first character that only appears in the string once
+# Upper and lowercase chars are considered the same character, but fn...
+    # ... should return the correct case for initial letter
+# Edge cases:
+    # If all characters repeat, return ""
+
+def first_non_repeating_letter(input: str) -> str:
+    # Dummy placeholder
+    return ""
+
