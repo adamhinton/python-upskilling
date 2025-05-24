72c5e9523457378be1358dfc6eb1669ec080c4c1
adamhinton <adam.d.hinton@gmail.com>
2025-05-23 20:34:25 -0400

    Best solution

--- problems/section-1/container-with-most-water.py
+++ problems/section-1/container-with-most-water.py
@@ -14,25 +14,44 @@
 
 from typing import List
 
-def maxContainerWater(arr: List[int]) -> int:
+def maxContainerWater(height: List[int]) -> int:
+
+    if len(height) == 0:
+        return 0
+
     max_area = 0
-    left, right = 0, len(arr) - 1
+    left, right = 0, len(height) - 1
+
+    while right > left:
+        left_height = height[left]
+        right_height = height[right]
+
+        current_area = (right - left) * min(left_height, right_height)
+        max_area = max(current_area, max_area)
+
+        if left_height < right_height:
+            left += 1
+
+        else:
+            right -= 1
+
 
     return max_area
 
 print(maxContainerWater([1,8,6,2,5,4,8,3,7])) # 49
 print(maxContainerWater([1, 5, 6, 3, 4, 2])) # 12
 print(maxContainerWater([1])) #0
+print(maxContainerWater([1,1])) # 1
 
 # PLAN:
 # maxArea = 0
-# left, right = 0, len(arr) - 1
+# left, right = 0, len(height) - 1
 
 # while right > left:
-    # left_height = arr[left]
-    # right_height = arr[right]
+    # left_height = height[left]
+    # right_height = height[right]
 
-    # current_area is (right - left) * min (arr[left], arr[right])
+    # current_area is (right - left) * min (height[left], height[right])
     # max_area is now the greater of max_area and current_area
 
     # if left_height < right_height left++; else right--
