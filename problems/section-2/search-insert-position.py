249877179030db8ebcfd6515ca56ef0a112aa0ac
adamhinton <adam.d.hinton@gmail.com>
2025-06-07 22:50:41 -0400

    search-insert-position pseudocode

--- problems/section-2/search-insert-position.py
+++ problems/section-2/search-insert-position.py
@@ -9,3 +9,28 @@ from typing import List
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         return 0
+    
+# Thoughts:
+# Log(n) time (obviously)
+# Binary search through sorted array
+
+# PLAN:
+# left and right pointers
+
+# while low < high:
+    # mid = (high + low) // 2
+
+    # if nums[mid] == target:
+        # return mid -- happy path
+
+    # if nums[mid] > target:
+        # right = mid - 1
+
+    # else if nums[mid] < target and nums[mid + 1] <= target:
+        # left = mid + 1
+
+    # I think this means target is between nums[mid] and nums[mid + 1]
+    # else:
+        # Return mid + 1
+
+# Not sure how to handle item not existing (TODO)
