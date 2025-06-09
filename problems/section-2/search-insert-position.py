b99f21d1bdb5763e36d00a81118d92c14bcb6038
adamhinton <adam.d.hinton@gmail.com>
2025-06-09 02:45:57 -0400

    Updated pseudocode

--- problems/section-2/search-insert-position.py
+++ problems/section-2/search-insert-position.py
@@ -15,22 +15,20 @@ class Solution:
 # Binary search through sorted array
 
 # PLAN:
-# left and right pointers
+# low and high pointers
 
-# while low < high:
+# while low <= high:
     # mid = (high + low) // 2
 
     # if nums[mid] == target:
         # return mid -- happy path
 
     # if nums[mid] > target:
-        # right = mid - 1
+        # high = mid - 1
 
-    # else if nums[mid] < target and nums[mid + 1] <= target:
-        # left = mid + 1
+    # else if nums[mid] < target
+        # low = mid + 1
 
-    # I think this means target is between nums[mid] and nums[mid + 1]
-    # else:
-        # Return mid + 1
+# return low
 
 # Not sure how to handle item not existing (TODO)
