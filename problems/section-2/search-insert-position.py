bc72104e7ef0c0a66850ada4eb6bbe16e5326f33
adamhinton <adam.d.hinton@gmail.com>
2025-06-09 02:51:36 -0400

    Working solution. yay!

--- problems/section-2/search-insert-position.py
+++ problems/section-2/search-insert-position.py
@@ -8,7 +8,29 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return 0
+        low = 0
+        high = len(nums) -1
+
+        while low <= high:
+            mid = (high + low) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] > target:
+                high = mid - 1
+            
+            # This is the only possible remaining case,
+            # So could just be `else`
+            # But I like the elif for clarity
+            elif nums[mid] < target:
+                low = mid + 1
+
+            # This should never happen
+            else:
+                Exception("You suck!")
+                
+        return low
     
 # Thoughts:
 # Log(n) time (obviously)
@@ -32,3 +54,8 @@ class Solution:
 # return low
 
 # Not sure how to handle item not existing (TODO)
+
+print(Solution().searchInsert([1,3,5,6], 5))  # → 2
+print(Solution().searchInsert([1,3,5,6], 2))  # → 1
+print(Solution().searchInsert([1,3,5,6], 7))  # → 4
+print(Solution().searchInsert([1,3,5,6], 0))  # → 0
