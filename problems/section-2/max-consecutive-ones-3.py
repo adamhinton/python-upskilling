94128287bbe3651c2a3c0ff85696b9f0949dfe9d
adamhinton <adam.d.hinton@gmail.com>
2025-06-18 00:03:03 -0400

    Skeleton solution

---
+++ problems/section-2/max-consecutive-ones-3.py
@@ -0,0 +1,11 @@
+from typing import List
+
+# Given a binary array nums and an integer k, 
+# return the maximum number of consecutive 1's in the array 
+#   if you can flip at most k 0's.
+
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        pass
