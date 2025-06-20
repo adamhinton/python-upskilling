17c8a168937ee618227e6450ebe27fbdeb81a86d
adamhinton <adam.d.hinton@gmail.com>
2025-06-19 21:54:30 -0400

    First attempt

--- problems/section-2/max-consecutive-ones-3.py
+++ problems/section-2/max-consecutive-ones-3.py
@@ -4,9 +4,26 @@ from typing import List
 # return the maximum number of consecutive 1's in the array 
 #   if you can flip at most k 0's.
 
+#https://leetcode.com/problems/max-consecutive-ones-iii/
+
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        pass
+        
+        num_zeros = 0
+        left = 0
+        max_len = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0: num_zeros += 1
+
+            while num_zeros > k:
+                if nums[left] == 0:
+                    num_zeros -=1
+                left +=1
+
+            max_len = max(max_len, right - left +1)
+
+        return max_len
 
 # PLAN
 # Two pointers sliding window
