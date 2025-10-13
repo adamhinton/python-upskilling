3679a79ee283ee80bf7f29246439e46f3b2f1716
adamhinton <adam.d.hinton@gmail.com>
2025-10-13 13:21:42 -0400

    Install ts

---
+++ problems/treehouse-TS-prep/2418-SortThePeople.ts
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/sort-the-people/description/
+
+// array of strings names
+// array heights --- distinct pos ints
+// both of length n
+
+// For each index i, names[i] and heights[i] denote name and height of ith person
+
+// Return names sorted in desc order by people's heights
+
+// First idea:
+// Make a `people` array
+// Loop over names, populating people with names and heights
+// Sort `people`
+// O (n log n) complexity
+
+// Anything better?
+
+
+const sortPeople = (names: string[], heights: number[]):  string[] =>{
+
+    const solution: string[] = []
+
+    type Person = {
+        name: string;
+        height: number;
+    }
+
+    const people: Person[] = names
+    .map((personName, i) =>{
+        return {name: personName, height: heights[i]}
+    })
+    .sort((p1, p2) =>{
+        return p1.height + p2.height
+    })
+
+    for( const person of people) {
+        solution.push(person.name)
+    }
+
+    return solution;
+};
+
+console.log(sortPeople(["Mary","John","Emma"], [180,165,170])) // Mary Emma John
+console.log(sortPeople(["Alice","Bob","Bob"],[155,185,150] )) // Bob Alice Bob
---
+++ problems/treehouse-TS-prep/CustomSort.ts
@@ -0,0 +1,11 @@
+// given arr1 and arr2, 
+// arr2's elements are distinct
+// all elements in arr2 are also in arr1
+
+// Sort arr1 such that the relative ordering of arr1 is the same as arr2.
+// Eleemnts that aren't in arr2 should be at the end of arr1 in ascending order
+
+
+
+const bob = 5;
+console.log (bob + 5);
