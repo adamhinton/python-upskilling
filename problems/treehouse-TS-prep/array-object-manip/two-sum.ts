428fe2941dd99865bc8a7f730218a6fbd90000de
adamhinton <adam.d.hinton@gmail.com>
2025-10-16 15:02:09 -0400

    plan

--- problems/treehouse-TS-prep/array-object-manip/two-sum.ts
+++ problems/treehouse-TS-prep/array-object-manip/two-sum.ts
@@ -8,6 +8,13 @@
 
 // You can return the answer in any order.
 
+// PLAN:
+// Make numIndexes object
+// For num in nums,
+    // needed = target - num
+    // If needed in numIndexes, return those two indices
+    // numIndexes[num] = i
+
 function twoSum(nums: number[], target: number): [number, number] {
     
     return [-1, -1]
