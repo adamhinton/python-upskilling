9b502544ea85e2218a17e490ceabf2208441ed08
adamhinton <adam.d.hinton@gmail.com>
2025-10-16 15:00:07 -0400

    skeleton fn

---
+++ problems/treehouse-TS-prep/array-object-manip/two-sum.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/two-sum/description/
+
+// Given an array of integers nums and an integer target, 
+// return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, 
+// and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+function twoSum(nums: number[], target: number): [number, number] {
+    
+    return [-1, -1]
+};
