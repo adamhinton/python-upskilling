2ee04c1c85ae01ffcfca8644b7394baa948629a8
adamhinton <adam.d.hinton@gmail.com>
2025-10-16 20:38:07 -0400

    Skeleton fn

--- problems/treehouse-TS-prep/array-object-manip/InterSectionOfArrays.ts
+++ problems/treehouse-TS-prep/array-object-manip/InterSectionOfArrays.ts
@@ -1,1 +1,16 @@
-https://leetcode.com/problems/intersection-of-two-arrays/description/
+// https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+// 349. Intersection of Two Arrays
+
+// Given two integer arrays nums1 and nums2, 
+// return an array of their intersection. 
+// Each element in the result must be unique
+//      and you may return the result in any order.
+
+
+// PLAN
+// This should be quite easy, unless I'm missing something
+
+function intersection(nums1: number[], nums2: number[]): number[] {
+    
+};
