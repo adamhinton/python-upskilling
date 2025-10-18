a31e85716cb7626d282f25b0f7e93fd8b63730ee
adamhinton <adam.d.hinton@gmail.com>
2025-10-18 00:00:16 -0400

    plan

--- problems/treehouse-TS-prep/stacks/isValidParentheses.ts
+++ problems/treehouse-TS-prep/stacks/isValidParentheses.ts
@@ -11,6 +11,43 @@
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
 
+// PLAN
+// Straightforward stack stuff
+
+// Make pairs Map of closeParen : openParen, three pairs
+
+// const parensStack: string[] = []
+
+// for paren in parens:
+// if (pairs.has(paren))) it's a closing paren
+    // top is parensStack.pop()
+    // if it's the valid opener, continue
+    // if not return false
+
+// else: it's an opening paren
+    // parensStack.push(paren)
+
+// return parensStack.length === 0
+
+
+
 function isValid(s: string): boolean {
-    
+    const pairs = new Map([
+        ['}', '{'],
+        [')', '('],
+        [']', '[']
+    ]
+    )
+
+    const parenStack: string[] = []
+
+    for (const paren of s){
+        if(pairs.has(paren)){ // it's a closing paren
+            
+        } 
+
+    }
+
+
+    return true
 };
