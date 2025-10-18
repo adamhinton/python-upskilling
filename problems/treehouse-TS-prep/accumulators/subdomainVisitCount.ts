20d7dd110181db31cf94fa69b6da8ae0ae6b1058
adamhinton <adam.d.hinton@gmail.com>
2025-10-18 02:33:33 -0400

    Skeleton fn

---
+++ problems/treehouse-TS-prep/accumulators/subdomainVisitCount.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/subdomain-visit-count/description/
+
+// I usually copy the description here but it's pretty long
+// See the problem URL for instructions
+
+// PLAN
+// Should be O(n * l), with L being the length of domain strings
+// Need: hash tables, accumulators strings, backwards for loops
+
+// const counts : Record<string, number> >>> {domain.google.com : 80000}
+
+// for loop of cpdomains:
+
+
+
+
+function subdomainVisits(cpdomains: string[]): string[] {
+    return []
+};
