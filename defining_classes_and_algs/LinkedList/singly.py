6ef54889f883587d3085fb5b8498be54611ca9bc
adamhinton <adam.d.hinton@gmail.com>
2025-10-29 19:22:48 -0400

    linkedlist

--- defining_classes_and_algs/LinkedList/singly.py
+++ defining_classes_and_algs/LinkedList/singly.py
@@ -15,3 +15,27 @@ class Node(object):
     def set_data(self, data):
         self.data = data
 
+class LinkedList (object):
+    def __init__(self, r = None):
+        self.root = r
+        self.size = 0
+
+    def get_size (self):
+        return self.size
+    
+    def add (self, data):
+        new_node = Node(data, self.root)
+        self.root = new_node
+        self.size +=1
+
+    def remove (self, data):
+        this_node = self.root # the current node we're looking at
+        prev_node = None # starts at rot
+        while this_node:
+            if this_node.get_data() == data:
+                prev_node.set_next(this_node.get_next())
+            else:
+                self.root = this_node
+                self.size -= 1
+                return True
+
