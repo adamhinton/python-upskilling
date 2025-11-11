a55c1c396a1d8dba75bccea7bf4cd46a7b78de4a
adamhinton <adam.d.hinton@gmail.com>
2025-11-11 17:39:10 -0500

    First attempt - my math seems to be off by one

---
+++ problems/queues/num_recent_calls.py
@@ -0,0 +1,34 @@
+# 933. Number of Recent Calls
+# https://leetcode.com/problems/number-of-recent-calls/description/
+
+# You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+# Implement the RecentCounter class:
+
+# RecentCounter() Initializes the counter with zero recent requests.
+# int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+# It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.pings = deque([])
+        
+
+    def ping(self, t: int) -> int:
+        self.pings.append(t)
+        while t - self.pings[-1] >= 3000:
+            self.pings.pop()
+        return len(self.pings)
+
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+solution = RecentCounter()
+print(solution.ping(1), solution.ping(100), solution.ping(3001), solution.ping(3002))
