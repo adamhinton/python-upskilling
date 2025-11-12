135774da2cc49a34a0209fbf8617f25015f43a86
adamhinton <adam.d.hinton@gmail.com>
2025-11-12 12:47:17 -0500

    Notes on improvement

--- problems/queues/dota2_senate.py
+++ problems/queues/dota2_senate.py
@@ -3,6 +3,9 @@
 
 # Problem description is too long to include here, but please see above link
 
+# Notes: The runtime of this could be improved, especially when I make senate in to a deque. Can revisit this another time.
+# Even despite this, my solution scored surprisingly well on runtime.
+
 from typing import Literal
 from collections import deque
 
@@ -21,7 +24,7 @@ class Solution:
 
         for senator_char in senate:
             senator_numbers[senator_char] += 1
-            senate_dq.appendleft(senator_char)
+            senate_dq.append(senator_char)
 
         can_eliminate = {
             "R": 0,
@@ -29,14 +32,14 @@ class Solution:
         }
 
         while True:
-            senator_party = senate_dq.pop()
+            senator_party = senate_dq.popleft()
             if can_eliminate[senator_party] > 0:
                 can_eliminate[senator_party] -= 1
                 senator_numbers[senator_party] -=1
             else:
                 opposite = opposite_party(senator_party)
                 can_eliminate[opposite] += 1
-                senate_dq.appendleft(senator_party)
+                senate_dq.append(senator_party)
 
             if senator_numbers[senator_party] == len(senate_dq):
                 return "Dire" if senator_party == "D" else "Radiant"
