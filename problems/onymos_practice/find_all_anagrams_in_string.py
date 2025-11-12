7239aa60944524c67f9266eda435ed01fd112fe9
adamhinton <adam.d.hinton@gmail.com>
2025-11-12 17:23:29 -0500

    Skeleton FN

---
+++ problems/onymos_practice/find_all_anagrams_in_string.py
@@ -0,0 +1,15 @@
+# 438. Find all anagrams in a string
+# https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+
+# Given two strings s and p, 
+# return an array of all the start indices of p's anagrams in s. 
+# You may return the answer in any order.
+
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        return ['a']
+    
+
+# PLAN:
