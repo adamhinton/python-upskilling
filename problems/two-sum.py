66ed4307f384d8576a7b80a12d2d2364681e40fb
adamhinton <adam.d.hinton@gmail.com>
2025-11-12 12:55:42 -0500

    skeleton fn

--- problems/two-sum.py
+++ problems/two-sum.py
@@ -15,25 +15,3 @@ from typing import List, Tuple, Dict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> Tuple[int, int]:
-        seen_numbers: Dict[int, int] = {}
-
-        for i, num in enumerate(nums):
-            complement = target - num
-
-            if complement in seen_numbers:
-                return (i, seen_numbers[complement])
-            
-            seen_numbers[num] = i
-
-        raise ValueError("No solution found")    
-
-# PLAN
-
-# value, index
-# seen_numbers: Dict[int, int] = {}
-
-# for i, num in enumerate(nums):
-    # complement is target - num
-    # if seen_numbers has needed num, return (i, seen_numbers[complement])
-
-    # seen_numbers[num] = i
