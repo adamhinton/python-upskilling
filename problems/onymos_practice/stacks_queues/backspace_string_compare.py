84b9fbdb06f214d49679f9ca0111b3358032fe88
adamhinton <adam.d.hinton@gmail.com>
2025-11-13 18:39:03 -0500

    Final answer. All tests passing. Yay

--- problems/onymos_practice/stacks_queues/backspace_string_compare.py
+++ problems/onymos_practice/stacks_queues/backspace_string_compare.py
@@ -5,3 +5,40 @@
 # '#' means a backspace character.
 
 # Note that after backspacing an empty text, the text will continue empty.
+
+from collections import deque
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def helper(my_string: str) -> str:
+            s_stack = deque([])
+
+            for char in my_string:
+                if char == "#" and s_stack:
+                    s_stack.pop()
+                elif char != "#":
+                    s_stack.append(char)
+                # Does nothing if char is # and s_stack is empty; this is intended
+            
+            return "".join(s_stack)
+
+        s_backspaced = helper(s)
+        t_backspaced = helper(t)
+
+        return s_backspaced == t_backspaced
+            
+# PLAN
+# Use stack
+
+# t_stack, s_stack = deque([])
+
+# Helper function that does this:
+# for char in s_stack and t_stack:
+    # if char == "#":
+        # stack.pop()
+    # else:
+        # append char to stack
+    # return stack.join()
+
+# Perform helper function on both
+# Compare stacks
