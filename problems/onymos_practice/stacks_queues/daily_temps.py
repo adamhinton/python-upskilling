75c20b1739e58caa7db4c34eead03a901fa30eeb
adamhinton <adam.d.hinton@gmail.com>
2025-11-13 19:40:06 -0500

    First attempt

--- problems/onymos_practice/stacks_queues/daily_temps.py
+++ problems/onymos_practice/stacks_queues/daily_temps.py
@@ -12,6 +12,17 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        solution = [0] * len(temperatures)
+        mono_stack = []
+
+        for idx, temp in temperatures[::-1]:
+            while mono_stack and temperatures[mono_stack[-1]] <= temp:
+                mono_stack.pop()
+
+            solution[idx] = 0 if not mono_stack else (mono_stack[-1] - idx)
+
+            mono_stack.append(idx)
+
         return [0]
     
 # PLAN:
@@ -31,4 +42,9 @@ class Solution:
     # If no temps left in stack, solution[idx] = 0 >>> don't need to do this since it's actually 0, but I like the clarity
 
     # push idx in to mono_stack
+
+solution = Solution()
+print(solution.dailyTemperatures([73,74,75,71,69,72,76,73])) # [1,1,4,2,1,1,0,0]
+print(solution.dailyTemperatures([30,40,50,60])) # [1,1,1,0]
+print(solution.dailyTemperatures([30,60,90])) # [1,1,0]
 
