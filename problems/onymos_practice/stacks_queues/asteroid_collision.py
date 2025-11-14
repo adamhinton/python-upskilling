cc6fe9a9d0ab30898d91a2944e521490e3f8308a
adamhinton <adam.d.hinton@gmail.com>
2025-11-13 20:32:13 -0500

    Plan

--- problems/onymos_practice/stacks_queues/asteroid_collision.py
+++ problems/onymos_practice/stacks_queues/asteroid_collision.py
@@ -17,10 +17,39 @@ from typing import List
 
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        solution = []
+        solution = [] # stack
 
         return solution
     
 # PLAN
-
 # O(n), one pass
+# Need: Stack
+
+# Make a stack. Positive asteroids move up the stack towards the top,
+# negative asteroids move down the stack towards the bottom.
+# Could also move the opposite way but I find this more intuitive.
+
+# NOTES:
+    # Asteroids' values will never initially be 0
+
+# for ast in asteroids:
+    # if not solution, add ast to solution
+        # Continue
+
+    # if ast is positive:
+        # solution.append(ast)
+        # continue
+
+    # If ast is negative:
+        # while the top asteroid is positive:
+            # if abs(ast) > abs(top_ast):
+                # solution.pop() # top asteroid explodes from stack
+            # elif abs(ast) < abs(top_ast):
+                # ast explodes; continue
+            # else: >>> they're equal and annihilate each other
+                # solution.pop()
+                # continue
+                
+# return solution
+
+
