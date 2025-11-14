fb633d9c2c2fc9c30af75b9844ef2ef700d7e08b
adamhinton <adam.d.hinton@gmail.com>
2025-11-13 20:26:13 -0500

    Starting asteroid collisions LC #735

---
+++ problems/onymos_practice/stacks_queues/asteroid_collision.py
@@ -0,0 +1,26 @@
+# 735. Asteroid Collision
+# https://leetcode.com/problems/asteroid-collision/description/
+
+# We are given an array asteroids of integers representing asteroids in a row.
+# The indices of the asteroid in the array represent their relative position in space.
+
+# For each asteroid, the absolute value represents its size, 
+# and the sign represents its direction (positive meaning right, negative meaning left).
+# Each asteroid moves at the same speed.
+
+# Find out the state of the asteroids after all collisions. 
+# If two asteroids meet, the smaller one will explode.
+# If both are the same size, both will explode. 
+# Two asteroids moving in the same direction will never meet.
+
+from typing import List
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        solution = []
+
+        return solution
+    
+# PLAN
+
+# O(n), one pass
