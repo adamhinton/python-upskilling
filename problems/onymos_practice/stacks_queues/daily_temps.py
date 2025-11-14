e6f5eca48286894299b184fcdf6213dbfe43ad63
adamhinton <adam.d.hinton@gmail.com>
2025-11-13 19:29:28 -0500

    Plan

--- problems/onymos_practice/stacks_queues/daily_temps.py
+++ problems/onymos_practice/stacks_queues/daily_temps.py
@@ -8,6 +8,27 @@
 #   to get a warmer temperature. 
 # If there is no future day for which this is possible, keep answer[i] == 0 instead.
 
+from typing import List
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         return [0]
+    
+# PLAN:
+# O(n)
+
+# Need:
+# Monotonic stack to track indices in decreasing order
+
+# solution = []
+# mono_stack = []  >>> initialize with all zeros
+
+# for idx, temp in temperatures[::-1]
+    # while mono_stack and temperatures[mono_stack[-1]] <= temp:
+        # mono_stack.pop()
+    
+    # If any temps left in stack, solution[idx] = mono_stack[-1] - idx
+    # If no temps left in stack, solution[idx] = 0 >>> don't need to do this since it's actually 0, but I like the clarity
+
+    # push idx in to mono_stack
+
