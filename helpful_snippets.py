81dea4368e181d10890360ad22456404d063fa7c
adamhinton <adam.d.hinton@gmail.com>
2025-11-14 21:33:44 -0500

    better formatting

--- helpful_snippets.py
+++ helpful_snippets.py
@@ -6,9 +6,9 @@ for a,b in pairwise(dirs):
 # This will be: 
 # : up (-1, 0), right (0, 1), down (1, 0), and left (0, -1).
 
-            # for a,b in pairwise(dirs):
-            #     x, y = i+a, j+b
-            #     if (0<=x <= m) and (0 <= y <= n) and grid[x][y] == "1":
-            #         recursive_helper(x, y)
+    # for a,b in pairwise(dirs):
+    #     x, y = i+a, j+b
+    #     if (0<=x <= m) and (0 <= y <= n) and grid[x][y] == "1":
+    #         recursive_helper(x, y)
 
             
