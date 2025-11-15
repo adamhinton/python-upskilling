a4f374fae50ac7acac7d71bb2907555892c5135e
adamhinton <adam.d.hinton@gmail.com>
2025-11-14 21:06:57 -0500

    Implemented graphs successfully

--- problems/onymos_practice/graphs/implement_graphs.py
+++ problems/onymos_practice/graphs/implement_graphs.py
@@ -42,8 +42,29 @@ class Graph:
 
         return paths
     
-    def get_shortest_path(self, start, end, path=[]:
-        pass
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+        
+        if start not in self.graph_dict: # Doesn't fly to any destination
+            return None
+        
+        if start == end:
+            return path
+        
+        shortest_path = None # We don't know at the beginning what the shortest path is
+        
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if shortest_path is None or len(sp) < len(shortest_path):
+                    shortest_path = sp
+
+        return shortest_path
+
+                
+        
+        # Would you not just call get_paths here and return the shortest array out of the answers?
+    
         
     
 
@@ -62,6 +83,8 @@ if __name__ == "__main__":
 
     route_graph = Graph(routes)
 
-    start = "Mumbai"
+    start = "Paris"
     end = "New York"
-    print(f"\n Paths between {start} and {end}: ", route_graph.get_paths(start, end))
+    print(f"\n Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start, end))
+
+
