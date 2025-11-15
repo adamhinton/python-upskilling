4faf8c80c5d5e0b8c34f59df7115bb216e07b76c
adamhinton <adam.d.hinton@gmail.com>
2025-11-14 21:36:20 -0500

    All tests passing. Yay!

--- problems/onymos_practice/graphs/number_of_islands.py
+++ problems/onymos_practice/graphs/number_of_islands.py
@@ -18,30 +18,6 @@
 # ]
 # Output: 1
 
-# PLAN
-# NEED: recursion, graph, DFS, BFS too I think?
-# Time complexity: O(n) unless I'm much mistaken
-    # Both the helper and the main loop will only touch each item once
-
-# num_islands = 0
-
-# Loop through each item
-# WHen you first encounter a 1 in this main loop, islands++
-#   If value == 1:
-    # recursive_helper(current_coordinates)
-
-
-# def recursive_helper(coordinates)
-    # Not sure exactly what this should take in; coordinates maybe?
-
-    # grid[coordinates] = 0
-
-    # looping over the four potential neighbors:
-        # If neighbor exists and has value 1:
-            # recursive_helper(neighbor's coordinates)
-
-
-# return num_islands
 
 from typing import List
 from itertools import pairwise
@@ -90,3 +66,29 @@ print(solution.numIslands([
   ["0","0","0","1","1"]
 ])) # 3
     
+
+
+# PLAN
+# NEED: recursion, graph, DFS, BFS too I think?
+# Time complexity: O(n) unless I'm much mistaken
+    # Both the helper and the main loop will only touch each item once
+
+# num_islands = 0
+
+# Loop through each item
+# WHen you first encounter a 1 in this main loop, islands++
+#   If value == 1:
+    # recursive_helper(current_coordinates)
+
+
+# def recursive_helper(coordinates)
+    # Not sure exactly what this should take in; coordinates maybe?
+
+    # grid[coordinates] = 0
+
+    # looping over the four potential neighbors:
+        # If neighbor exists and has value 1:
+            # recursive_helper(neighbor's coordinates)
+
+
+# return num_islands
