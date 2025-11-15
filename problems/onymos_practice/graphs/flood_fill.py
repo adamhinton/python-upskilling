5e22a5269fa8ca546aa4bab7b6087b357a3a4edf
adamhinton <adam.d.hinton@gmail.com>
2025-11-14 22:08:29 -0500

    Plan

---
+++ problems/onymos_practice/graphs/flood_fill.py
@@ -0,0 +1,44 @@
+# 733. Flood Fill
+# https://leetcode.com/problems/flood-fill/description/
+
+# You are given an image represented by an m x n grid of integers image, 
+#   where image[i][j] represents the pixel value of the image. 
+# You are also given three integers sr, sc, and color.
+#  Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+
+# To perform a flood fill:
+
+# Begin with the starting pixel and change its color to color.
+# Perform the same process for each pixel that is directly adjacent 
+#   (pixels that share a side with the original pixel,either horizontally or vertically) 
+#   and shares the same color as the starting pixel.
+
+# Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+# The process stops when there are no more adjacent pixels of the original color to update.
+# Return the modified image after performing the flood fill.
+
+from typing import List
+from itertools import pairwise
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        return [[]]
+
+# PLAN
+# O(n)
+# DFS
+# Graph
+# Recursion
+
+# original_color = image[sr][sc]
+# new_color = color >>> less confusing naming
+
+# Start at specified coordinates. 
+
+# def recursive_helper(i, j):
+    # image[i][j] = new_color
+    # for pixel in up down left right (if they exist):
+        # if pixel color == original color, recursive_helper(pixel's coordinates)
+
+# Starting at original coordinates:
+    # recursive_helper(sr, sc)
