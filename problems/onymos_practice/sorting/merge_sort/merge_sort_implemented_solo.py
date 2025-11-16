0148ea0b1cff4df3976d320574b639d0f8e4fcb6
adamhinton <adam.d.hinton@gmail.com>
2025-11-16 11:19:28 -0500

    skeleton fn

---
+++ problems/onymos_practice/sorting/merge_sort/merge_sort_implemented_solo.py
@@ -0,0 +1,9 @@
+# Implementing this again for more practice
+
+from typing import List
+
+def merge_sort(arr: List[int]):
+
+arr_test = [2, 3, 5, 1, 7, 4, 4, 4, 2, 6, 0]
+merge_sort(arr_test)
+print(arr_test)
