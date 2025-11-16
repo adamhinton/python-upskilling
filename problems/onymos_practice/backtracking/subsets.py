79adfd295652ff8e4c7c69b3b5b8ed8c4238fee0
adamhinton <adam.d.hinton@gmail.com>
2025-11-15 23:59:45 -0500

    skeleton fn

---
+++ problems/onymos_practice/backtracking/subsets.py
@@ -0,0 +1,29 @@
+# 78. Subsets
+# https://leetcode.com/problems/subsets/description/
+
+# Given an integer array nums of unique elements, 
+# return all possible subsets (the power set).
+
+# The solution set must not contain duplicate subsets. 
+# Return the solution in any order.
+
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        solution = []
+        
+        return solution
+    
+# PLAN:
+
+# NEED:
+    # Backtracking
+    # Recursion
+
+# Time complexity:
+    # a lot
+    # 2 ^ n I think
+
+# total_nums = len(nums)
+
