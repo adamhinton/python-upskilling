d6992d8264fd84921c7fb5af2fb3a576b9042b6f
adamhinton <adam.d.hinton@gmail.com>
2025-11-17 18:06:08 -0500

    Plan

--- problems/onymos_practice/stacks_queues/asteroid_collision_dupe.py
+++ problems/onymos_practice/stacks_queues/asteroid_collision_dupe.py
@@ -14,8 +14,6 @@
 # If both are the same size, both will explode. 
 # Two asteroids moving in the same direction will never meet.
 
-
-
 from typing import List
 
 class Solution:
@@ -23,3 +21,48 @@ class Solution:
         solution = []
 
         return solution
+    
+# PLAN:
+# O(n) time, O(n) memory
+    # Could maybe make this O(1) memory by modifying asteroids in place and returning it
+    # Edit: That might run in to weirdness with modifying an array while looping over it, nah
+    # It's max 1000 asteroids, we don't have to worry about space
+
+# Need:
+    # monotonic stack
+
+# Concept:
+    # This simulation is like a literal physical stack:
+    # heavier asteroids (negative ints) fall down
+    # lighter asteroids (positive ints) rise up
+    # Note that asteroids[i] != 0
+
+# for ast in asteroids:
+    # if not solution, add asteroid to solution
+
+    # if ast is positive:
+        # add ast to solution
+
+    # if it's negative:
+        # while top_asteroid is positive:
+            # if abs(ast) > abs(top_asteroid):
+                # solution.pop()
+                # continue
+                # if solution is None or new top_asteroid is negative:
+                    # solution.append(ast)
+                    # break
+            # elif their abs values are equal:
+                # solution.pop()
+                # So, remove top asteroid from stack and don't add ast, they both exploded
+
+            # elif abs(ast) < abs(top_asteroid):
+                # break
+                # Aka ast explodess
+
+        
+
+solution = Solution()
+print(solution.asteroidCollision([5,10,-5])) # [5,10]
+print(solution.asteroidCollision([8,-8])) # []
+print(solution.asteroidCollision([10,2,-5])) # [10]
+print(solution.asteroidCollision([3,5,-6,2,-1,4])) # [-6, 2, 4]
