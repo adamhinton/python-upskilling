09bba0fccb146f892718f4ad67d7c7afc2f53c37
adamhinton <adam.d.hinton@gmail.com>
2025-11-17 18:56:55 -0500

    First attempt. Example tests passing

--- problems/onymos_practice/arrays_etc/max_consecutive_ones_iii_dupe.py
+++ problems/onymos_practice/arrays_etc/max_consecutive_ones_iii_dupe.py
@@ -12,6 +12,41 @@ from typing import List
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         max_length = 0
+        running_num_zeros = 0
+
+        if len(nums) == 1:
+            return 0 if (k == 0 and nums[0] == 0) else 1
+        
+        left, right = 0, 1
+        
+        if nums[left] == 0: 
+            running_num_zeros += 1 # we'll deal with nums[right] inside the loop
+
+        while right < len(nums):
+            num_left, num_right = nums[left], nums[right]
+
+            if num_right == 0:
+                running_num_zeros += 1
+
+            while running_num_zeros > k:
+                if num_left == 0:
+                    running_num_zeros -= 1
+                left+=1
+                num_left = nums[left]
+
+            # Now we know it's a valid window
+            current_length = right - left + 1
+            max_length = max(current_length, max_length)
+            right +=1
+
+        return max_length
+
+
+
+
+        
+        
+
 
         return max_length
     
@@ -21,6 +56,24 @@ class Solution:
 
 # Need:
 # Two pointers, start at index 0 and 1
+
+# running_num_zeros = 0
+# left, right = 0, 1
+# if nums[left] == 0, running_num_zeros ++
+
+# Sliding window pattern:
+    # while right < len(nums) >>> don't think it would help to do like `and left < len(nums) - k`
+        # if nums_right] == 0, running_num_zeros++
+
+        # Don't believe we need to make sure left never equals right
+        # window between nums left and nums right 
+        # While there are too many zeros (aka we can't flip them all):
+            # if nums[left] == 0, decrement running_num_zeros
+            # incrememt left
+
+    # Now we know it's a valid window
+    # max_length = max(right - left, max_length)
+    # right++
 
     
 solution = Solution()
