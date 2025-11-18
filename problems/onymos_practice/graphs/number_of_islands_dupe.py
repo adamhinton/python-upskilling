eca559a7274f95ca585fb19bdb3eeec4fc506d84
adamhinton <adam.d.hinton@gmail.com>
2025-11-18 01:04:39 -0500

    plan

--- problems/onymos_practice/graphs/number_of_islands_dupe.py
+++ problems/onymos_practice/graphs/number_of_islands_dupe.py
@@ -22,10 +22,37 @@
 # Output: 1
 
 from typing import List
-from itertools import pairwiseπ
+from itertools import pairwise
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         num_islands = 0
 
         return num_islands
+    
+# PLAN:
+    # Time: Something nasty, m * n * average length of islands I think
+    # Space: O(1)
+
+    # Broad plan:
+        # for each item, if it is 1,
+            # Change it to 0, num_islands ++
+            # for each square up, left, down, right (if they exist)
+                # call recursive helper(row, col) 
+        
+        # recursive helper(row, col):
+                # This sets a square's value to 0, then searches for more connecting land and sets those to 0 too.
+                # Should recurse all the way through an island then stop.
+                # Converts land from 1 to 0 so it doesn't get visited again.
+            # if val is 0, return
+            # if val == 1:
+                # change val to 0
+                # for each orthogonal square (that exist):
+                    # recursive helper on that orthogonal square
+
+# num_islands = 0
+
+
+
+
+# return num_islands
