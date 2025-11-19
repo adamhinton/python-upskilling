70c54d8670eb94f8bbbfb5afb9f01f2add17aff5
adamhinton <adam.d.hinton@gmail.com>
2025-11-18 23:41:07 -0500

    Plan

--- problems/onymos_practice/backtracking/num_enclaves.py
+++ problems/onymos_practice/backtracking/num_enclaves.py
@@ -14,3 +14,45 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         num_enclaves = 0
         return num_enclaves
+    
+# PLAN
+# Time: O(m * n * ... length of average island, I think)
+# space: O(n)
+# Need:
+    # Recursion
+    # backtracking
+    # `seen` set for current path coordinates
+    # DFS
+
+# m, n = len(grid), len(grid[0])
+# num_enclaves = 0
+
+# def recursive_helper(row, col):
+    # val = grid[row][col]
+    # if val == 0: return >> or check for this before calling recursive_helper
+    # if it's a border square, is_enclave = False
+    
+    # for each adjacent square:
+        # call recursive_helper(adjacentcoords) if:
+            # square exists, and
+            # maybe check here whether its val is 0?
+
+
+# for i, j in whatever:
+    # if val is 1:
+        # is_enclave = True >> recursive helper will change this if it finds a border square with val 1
+
+        # recursive_helper(i, j)
+            # set is_enclave to false if it's border square
+            # set val to 0
+            # call itself on adjacent squares
+            # remove its coords from seen when done
+
+        # if is_enclave num_enclaves ++
+
+# return num_enclaves
+
+
+solution = Solution()
+print(solution.numEnclaves([[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]])) # 3
+print(solution.numEnclaves([[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]])) # 0
