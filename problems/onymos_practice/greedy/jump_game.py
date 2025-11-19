f630a62f25ca3b297549eb7e7b15c119956e5137
adamhinton <adam.d.hinton@gmail.com>
2025-11-19 10:59:58 -0500

    Skeleton Fn

---
+++ problems/onymos_practice/greedy/jump_game.py
@@ -0,0 +1,17 @@
+# 55. Jump Game
+# Medium
+# https://leetcode.com/problems/jump-game/description/
+
+# You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+# Return true if you can reach the last index, or false otherwise.
+
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        return False
+    
+solution = Solution()
+print(solution.canJump([2,3,1,1,4])) # True
+print(solution.canJump([3,2,1,0,4])) # False
