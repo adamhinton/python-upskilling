1cf06c38a821880856a0d63d6f568f73eeb2ed11
adamhinton <adam.d.hinton@gmail.com>
2025-11-19 11:04:34 -0500

    Plan

--- problems/onymos_practice/greedy/jump_game.py
+++ problems/onymos_practice/greedy/jump_game.py
@@ -6,6 +6,11 @@
 
 # Return true if you can reach the last index, or false otherwise.
 
+# Constraints:
+
+# 1 <= nums.length <= 104
+# 0 <= nums[i] <= 105
+
 from typing import List
 
 class Solution:
@@ -15,3 +20,22 @@ class Solution:
 solution = Solution()
 print(solution.canJump([2,3,1,1,4])) # True
 print(solution.canJump([3,2,1,0,4])) # False
+
+# PLAN
+# Time: O(n)
+# Space: O(1)
+# Need:
+    # Greedy
+    # I think that's all
+
+# Idea: Moving the goalposts
+# If we can get to square at index i, we can get to any square before it.
+# So we start with the goal at nums[len(nums) -1]
+    # Then, loop backwards over index i
+    # if nums[i]'s val is high enough to jump to goal,
+        # goal = i
+
+# return goal == 0 >>> We've gotten to the front. Yay!
+
+# Possible optimizations:
+    # None immediately come to mind
