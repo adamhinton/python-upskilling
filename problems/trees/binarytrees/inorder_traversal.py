64b1915d053a67eb8b694ed21d839b6779dbf87e
adamhinton <adam.d.hinton@gmail.com>
2025-11-23 15:39:50 -0500

    All tests passing

--- problems/trees/binarytrees/inorder_traversal.py
+++ problems/trees/binarytrees/inorder_traversal.py
@@ -5,4 +5,25 @@
 
 # Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
- 
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        solution = []
+
+        def recursive_helper(root: Optional[TreeNode]):
+            if root is None:
+                return
+        
+            recursive_helper(root.left)
+            solution.add(root.val)
+            recursive_helper(root.right)
+
+        return solution
+        
