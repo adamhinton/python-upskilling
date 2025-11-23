acd29b6790cae7a520ce72b57a7f8d49c5dc7aee
adamhinton <adam.d.hinton@gmail.com>
2025-11-23 15:27:01 -0500

    All tests passing

--- problems/linkedlists/merge_two_sorted_linkedlists.py
+++ problems/linkedlists/merge_two_sorted_linkedlists.py
@@ -8,3 +8,40 @@
 
 # Return the head of the merged linked list.
 
+# Have to use the original nodes, can't make copies of them  
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while (list1 and list2):
+            if list1.val > list2.val:
+                tail.next = list2
+                list2 = list2.next
+
+            else: # list2.val is >= list1.val
+                tail.next = list1
+                list1 = list1.next
+
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        
+        elif list2:
+            tail.next = list2
+
+        return dummy.next
