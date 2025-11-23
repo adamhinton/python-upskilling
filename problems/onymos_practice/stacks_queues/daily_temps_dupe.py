7f53c85e0e96fec043a9ce859b7067ff47c34d8a
adamhinton <adam.d.hinton@gmail.com>
2025-11-23 00:32:58 -0500

    skeleton fn

--- problems/onymos_practice/stacks_queues/daily_temps_dupe.py
+++ problems/onymos_practice/stacks_queues/daily_temps_dupe.py
@@ -16,3 +16,9 @@ class Solution:
         solution = []
 
         return solution
+    
+
+solution = Solution()
+print(solution.dailyTemperatures([73,74,75,71,69,72,76,73])) # [1,1,4,2,1,1,0,0]
+print(solution.dailyTemperatures([30,40,50,60])) # [1,1,1,0]
+print(solution.dailyTemperatures([30,60,90])) # [1,1,0]
