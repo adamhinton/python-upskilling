4fecd894f7be356ddd2007f0edd7b2dbbc7b6110
adamhinton <adam.d.hinton@gmail.com>
2025-11-23 00:38:24 -0500

    Plan

--- problems/onymos_practice/stacks_queues/daily_temps_dupe.py
+++ problems/onymos_practice/stacks_queues/daily_temps_dupe.py
@@ -13,9 +13,32 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        solution = []
+        solution = [None for _ in range(len(temperatures))]
 
         return solution
+    
+# PLAN
+# Time: O(n) one pass
+# Space: O(n) - could maybe make it O(1) by modifying temps in place; seems unnecessary
+# Need:
+    # Monotonic stack - track next highest temperature
+    # Loop backwards over temps
+
+# solution = [] >> QUEUE of length len(temps)
+# temp_stack = [] >>> holds INDEXES, not temperatures
+
+# for idx, temp in (looping over temps backwards):
+    # while temp_stack has vals and temp > the top temp of the stack
+        # temp_stack.pop()
+
+    # if there are values in stack:
+        # days_waited = temp_stack[-1] - index
+        # solution.appendleft[days_waited] >>> building backwards
+
+    # else: >> no temps greater so far
+        # solution.appendleft(0)
+
+# return solution
     
 
 solution = Solution()
