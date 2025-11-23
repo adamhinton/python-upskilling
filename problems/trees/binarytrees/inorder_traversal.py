d9e0ad3529e6c3fa8be031672b488427325c8560
adamhinton <adam.d.hinton@gmail.com>
2025-11-23 15:34:00 -0500

    Starting inorder binary tree traversal

---
+++ problems/trees/binarytrees/inorder_traversal.py
@@ -0,0 +1,8 @@
+# 94. Binary Tree Inorder Traversal
+# Easy
+
+# https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
