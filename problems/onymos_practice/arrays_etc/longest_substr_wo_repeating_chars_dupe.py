34ee8611484c245ce3981253926d1e5038b99dcb
adamhinton <adam.d.hinton@gmail.com>
2025-11-25 09:00:26 -0500

    Starting max substring without repeating characters

---
+++ problems/onymos_practice/arrays_etc/longest_substr_wo_repeating_chars_dupe.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length = 0
+
+        return max_length
+
+    
+solution = Solution()
+
+print(solution.lengthOfLongestSubstring("abcabcbb")) # 3
+print(solution.lengthOfLongestSubstring("bbbbb")) # 1
+print(solution.lengthOfLongestSubstring("pwwkew")) # 3
