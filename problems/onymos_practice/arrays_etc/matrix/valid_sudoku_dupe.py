55b5e317a400adb618d0e6cd11de6c63de1ac321
adamhinton <adam.d.hinton@gmail.com>
2025-11-25 09:48:09 -0500

    Skeleton fn

---
+++ problems/onymos_practice/arrays_etc/matrix/valid_sudoku_dupe.py
@@ -0,0 +1,51 @@
+# 36. Valid Sudoku
+# Medium
+# Review later
+# https://leetcode.com/problems/valid-sudoku/description/
+
+# Determine if a 9 x 9 Sudoku board is valid. 
+#   Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+
+from collections import List
+from typing import Tuple, DefaultDict, Set
+
+# Doing this horror show more for comedy than anything
+# I miss TypeScript
+Row = Tuple[str, str, str, str, str, str, str, str, str]
+Board = Tuple[
+    Row, Row, Row, Row, Row, Row, Row, Row, Row
+]
+
+class Solution:
+    def isValidSudoku(self, board: Board) -> bool:
+
+        return True
+
+solution = Solution()
+print(solution.isValidSudoku([["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]])) # True
+
+print(solution.isValidSudoku([["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]])) # False
