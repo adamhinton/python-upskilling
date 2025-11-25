6b3a5c914d4f06e8c05f494fa8bac06e6c97a10c
adamhinton <adam.d.hinton@gmail.com>
2025-11-25 10:27:36 -0500

    Plan

--- problems/onymos_practice/arrays_etc/three_sum_dupe.py
+++ problems/onymos_practice/arrays_etc/three_sum_dupe.py
@@ -17,10 +17,38 @@
 from typing import List
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums:  List[int]) -> List[List[int]]:
+        if len(nums) == 3:
+            return [nums]
         solution = []
 
         return solution
+    
+# PLAN:
+# time: O(n ** 2)
+# Space: O(n)
+# Need:
+    # Sorting
+    # Two pointers (start and end)
+
+# Sort nums
+# solution = []
+
+# For idx in range(len(nums) -2): >>> stop when less than three nums left
+    # if previous num == num, continue >>> avoid dupes
+    # If num > 0: break
+
+    # left, right = idx + 1, len(nums) - 1
+
+    # while right > left and right val isn't a negative number:
+        # needed = 0 - num
+        # if right val + left val == needed: solution.append([num, left, right])
+        # If it's less than needed, left ++
+            # Keep doing this until we get a new left val, no dupes
+        # else it's more than needed, right--
+            # keep doing this until we get a new right val, no dupes
+
+    # return solution
 
 
 solution = Solution()
