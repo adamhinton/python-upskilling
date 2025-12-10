2c97f84d1c01fa1626e7043c76e505eb21acf7e0
adamhinton <adam.d.hinton@gmail.com>
2025-12-10 10:28:18 -0500

    Starting on merge sort (again for practice)

---
+++ problems/onymos_practice/sorting/merge_sort/sort_an_array_iv.py
@@ -0,0 +1,13 @@
+# Doing this again for practice
+
+from typing import List
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        return nums
+
+
+solution = Solution()
+print(solution.sortArray([5,2,3,1]))
+print(solution.sortArray([5,1,1,2,0,0]))
